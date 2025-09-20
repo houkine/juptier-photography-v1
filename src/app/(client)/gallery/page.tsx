f266160b1955773ddf8fb3a0dcdacc35f6b633c3
pan <houkine@outlook.com>
2025-09-20 15:26:50 +1000

    dev 02

--- src/app/(client)/gallery/page.tsx
+++ src/app/(client)/gallery/page.tsx
@@ -9,13 +9,16 @@ const Page = () => {
         {/* <div className="h-screen w-screen flex bg-[url('/images/gallery_image/bgimg1.jpg')] bg-cover pt-32 "> */}
             <ul className='w-full h-full overflow-y-auto'>
                 {gallery.map((set, index) => (
-                    <li className='flex flex-col pt-32 container mx-auto opacity-60 animate-fadeIn_galleryContent hover:opacity-100 transition duration-500' id={set.title} key={index}>
+                    <li className='flex flex-col my-16 container mx-auto' id={set.title} key={index}>
                         <h1 className='text-6xl tracking-widest text-gray-500'>{set.title}</h1>
                         <p className='text-xl tracking-wide text-gray-400 italic mt-3'>{set.content}</p>
                         <div className='flex justify-between mt-10 w-full space-x-5'>
                             {set.sub_gallery.map((photo, index) => (
-                                <div className='flex w-1/3'>
-                                    <img src={photo.src} alt={photo.alt} className='flex m-auto rounded-md cursor-pointer shadow-lg hover:shadow-2xl transition duration-200' onClick={() => alert('we are working on it')} key={index} />
+                                <div className='flex relative w-1/3 cursor-pointer'  key={index} onClick={()=>alert('photos')}>
+                                    <img src={photo.src} alt={photo.alt} className='flex m-auto rounded-md cursor-pointer shadow-lg hover:shadow-2xl transition duration-200' onClick={() => alert('we are working on it')} />
+                                    <div className='absolute w-full h-full flex bg-white opacity-0 hover:opacity-60 transition duration-1000'>
+                                        <p className='text-2xl font-bold m-auto'>Hello</p>
+                                    </div>
                                 </div>
                             ))}
                         </div>
